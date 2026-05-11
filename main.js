5e093fccf7bb098ad6e5dd614ec361378e8efde4
istvzsig <istvanzsigmond@aol.com>
2026-05-11 16:11:52 +0200

    add basic logging to main

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
 require("dotenv").config();
 
 const { Client, Collection, GatewayIntentBits } = require("discord.js");
+const logger = require("./src/utility/logger.js");
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds],
 });
-
 client.commands = new Collection();
+
+logger.info("Bot is starting...");
+
+process.on("unhandledRejection", (err) => {
+  logger.error("Unhandled promise rejection: " + err);
+});
 
 require("./src/handlers/eventHandler.js")(client);
 require("./src/handlers/commandHandler.js")(client);
