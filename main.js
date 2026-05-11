8f021a1c8b20fa33245c316fa4266c92d8c27b73
istvzsig <istvanzsigmond@aol.com>
2026-05-11 20:48:58 +0200

    add GuildMembers intent permission

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const { Client, Collection, GatewayIntentBits } = require("discord.js");
 const logger = require("./src/utility/logger.js");
 
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds],
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers],
 });
 client.commands = new Collection();
 
