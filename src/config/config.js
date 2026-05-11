0ac0b3fd0ea18c799720441ff8a37e79f76e5c1e
istvzsig <istvanzsigmond@aol.com>
2026-05-11 20:50:40 +0200

    update configuration

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,18 @@
 const CLIENT_ID = process.env.CLIENT_ID || process.argv[2];
+const GUILD_ID = process.env.GUILD_ID || process.argv[3];
+
 const BOT_PERMISSION_INTEGER = process.env.PERMISSION_INT || 8;
 const BOT_INVITE_URL = `https://discord.com/api/oauth2/authorize?client_id=${CLIENT_ID}&scope=bot%20applications.commands&permissions=${BOT_PERMISSION_INTEGER}`;
 
+const CommandMenu = {
+  ping: { command: "ping", desc: "Replies with Pong!" },
+  help: { command: "help", desc: "Displays bot menu." },
+};
+
 module.exports = {
   CLIENT_ID,
-  BOT_PERMISSION_INTEGER,
+  GUILD_ID,
   BOT_INVITE_URL,
+  BOT_PERMISSION_INTEGER,
+  commandMenu: CommandMenu,
 };
