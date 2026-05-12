0ee2dd428894c2c8513dd43a088c5dc49aa33e03
istvzsig <istvanzsigmond@aol.com>
2026-05-13 01:21:12 +0200

    add /use command to use items from inventory

---
+++ src/commands/economy/use.js
@@ -0,0 +1,57 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+const User = require("../../database/models/User");
+const registry = require("../../items/itemRegistry");
+const effectEngine = require("../../items/effectEngine");
+
+module.exports = {
+  category: "Economy",
+
+  data: new SlashCommandBuilder()
+    .setName("use")
+    .setDescription("Use an item from your inventory")
+    .addStringOption((opt) =>
+      opt.setName("item").setDescription("Item ID").setRequired(true),
+    ),
+
+  async execute(interaction, client) {
+    const itemId = interaction.options.getString("item");
+
+    const item = registry.get(itemId);
+
+    if (!item) {
+      return interaction.reply({
+        embeds: [client.embeds.error("Invalid item.")],
+        ephemeral: true,
+      });
+    }
+
+    const user = await User.findOne({
+      userId: interaction.user.id,
+    });
+
+    if (!user) return;
+
+    const invItem = user.inventory.find((i) => i.itemId === itemId);
+
+    if (!invItem) {
+      return interaction.reply({
+        embeds: [client.embeds.error("You don't own this item.")],
+        ephemeral: true,
+      });
+    }
+
+    // remove item if consumable
+    if (item.category === "consumable") {
+      user.inventory = user.inventory.filter((i) => i.itemId !== itemId);
+    }
+
+    await effectEngine.apply(user, item);
+
+    await user.save();
+
+    return interaction.reply({
+      embeds: [client.embeds.success(`✅ Used **${item.name}**`)],
+    });
+  },
+};
