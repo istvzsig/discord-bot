bfd2d88393d4d14bdd7e600050e211393b316daf
istvzsig <istvanzsigmond@aol.com>
2026-05-12 21:04:00 +0200

    delete unused getGuild function from ready event

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -3,19 +3,6 @@ const { GatewayIntentBits } = require("discord.js");
 const config = require("../../config/config.js");
 const logger = require("../../utility/logger.js");
 
-async function getGuild(client) {
-  // Ensure client was constructed with GatewayIntentBits.GuildMembers
-  const guild = await client.guilds.fetch(config.GUILD_ID);
-  // Fetch all members (requires Guild Members intent enabled in code & developer portal)
-
-  const members = await guild.members;
-  console.log(
-    `Guild: ${guild.name} (${guild.id}) -  fetched ${members.size} members`,
-  );
-
-  return { guild, members };
-}
-
 module.exports = {
   name: "clientReady",
   once: true,
