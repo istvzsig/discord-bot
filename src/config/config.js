578f07976dcf07de2f89f965694af94e64a1afa9
istvzsig <istvanzsigmond@aol.com>
2026-05-12 13:36:19 +0200

    delete unused commandMenu config

--- src/config/config.js
+++ src/config/config.js
@@ -1,14 +1,9 @@
-const CLIENT_ID = process.env.CLIENT_ID || process.argv[2];
-const GUILD_ID = process.env.GUILD_ID || process.argv[3];
+const CLIENT_ID = process.env.CLIENT_ID;
+const GUILD_ID = process.env.GUILD_ID;
 
 const BOT_TOKEN = process.env.TOKEN;
-const BOT_PERMISSION_INTEGER = process.env.PERMISSION_INT || 8;
+const BOT_PERMISSION_INTEGER = process.env.PERMISSION_INT;
 const BOT_INVITE_URL = `https://discord.com/api/oauth2/authorize?client_id=${CLIENT_ID}&scope=bot%20applications.commands&permissions=${BOT_PERMISSION_INTEGER}`;
-
-const commandMenu = {
-  ping: { command: "ping", desc: "Replies with Pong!" },
-  help: { command: "help", desc: "Displays bot menu." },
-};
 
 module.exports = {
   CLIENT_ID,
@@ -16,5 +11,4 @@ module.exports = {
   BOT_TOKEN,
   BOT_INVITE_URL,
   BOT_PERMISSION_INTEGER,
-  commandMenu,
 };
