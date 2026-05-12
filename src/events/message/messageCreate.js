746046867d22f9c9f3a393e190cb7146601c7f07
istvzsig <istvanzsigmond@aol.com>
2026-05-13 01:20:49 +0200

    add messageCreate event listener

---
+++ src/events/message/messageCreate.js
@@ -0,0 +1,24 @@
+const { addXp } = require("../../services/earnXp");
+const User = require("../../database/models/User");
+
+module.exports = {
+  name: "messageCreate",
+
+  async execute(message) {
+    if (message.author.bot) return;
+
+    const user = await User.findOneAndUpdate(
+      { userId: message.author.id },
+      {},
+      { upsert: true, new: true },
+    );
+
+    const result = await addXp(user, 10);
+
+    if (result.leveledUp) {
+      message.channel.send(
+        `🎉 ${message.author} leveled up to **${result.level}**!`,
+      );
+    }
+  },
+};
