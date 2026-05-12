56c58c8778314db5298cccc5c754439efddf078c
istvzsig <istvanzsigmond@aol.com>
2026-05-12 08:59:39 +0200

    add bot token to config

--- src/config/config.js
+++ src/config/config.js
@@ -1,10 +1,11 @@
 const CLIENT_ID = process.env.CLIENT_ID || process.argv[2];
 const GUILD_ID = process.env.GUILD_ID || process.argv[3];
 
+const BOT_TOKEN = process.env.TOKEN;
 const BOT_PERMISSION_INTEGER = process.env.PERMISSION_INT || 8;
 const BOT_INVITE_URL = `https://discord.com/api/oauth2/authorize?client_id=${CLIENT_ID}&scope=bot%20applications.commands&permissions=${BOT_PERMISSION_INTEGER}`;
 
-const CommandMenu = {
+const commandMenu = {
   ping: { command: "ping", desc: "Replies with Pong!" },
   help: { command: "help", desc: "Displays bot menu." },
 };
@@ -12,7 +13,8 @@ const CommandMenu = {
 module.exports = {
   CLIENT_ID,
   GUILD_ID,
+  BOT_TOKEN,
   BOT_INVITE_URL,
   BOT_PERMISSION_INTEGER,
-  commandMenu: CommandMenu,
+  commandMenu,
 };
