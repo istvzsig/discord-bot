c635a571045f65f07e12ed657d189c700cb62402
istvzsig <istvanzsigmond@aol.com>
2026-05-12 13:35:36 +0200

    refactor

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 
 const { Client, Collection, GatewayIntentBits } = require("discord.js");
+
+const { BOT_TOKEN } = require("./src/config/config.js");
 const logger = require("./src/utility/logger.js");
 
 const client = new Client({
@@ -10,11 +12,11 @@ client.commands = new Collection();
 
 logger.info("Bot is starting...");
 
+require("./src/handlers/commandHandler.js")(client);
+require("./src/handlers/eventHandler.js")(client);
+
+client.login(BOT_TOKEN);
+
 process.on("unhandledRejection", (err) => {
   logger.error("Unhandled promise rejection: " + err);
 });
-
-require("./src/handlers/eventHandler.js")(client);
-require("./src/handlers/commandHandler.js")(client);
-
-client.login(process.env.TOKEN);
