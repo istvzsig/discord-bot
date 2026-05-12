4deb32d9278f41851f06bfe9b9efc48f751655c0
istvzsig <istvanzsigmond@aol.com>
2026-05-13 00:37:32 +0200

    update db helper

--- src/database/helper.js
+++ src/database/helper.js
@@ -1,7 +1,9 @@
 async function getUser(client, userId) {
+  if (!client.db || !client.db.users) {
+    throw new Error("DB not initialized on client.db");
+  }
+
   return await client.db.users.ensure(userId);
 }
 
-module.exports = {
-  getUser,
-};
+module.exports = { getUser };
